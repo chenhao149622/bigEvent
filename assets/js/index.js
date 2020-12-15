2e2b05e14963750e1b912dfcbecd436a5b6bccf2
chenchenhao <1561528825@qq.com>
2020-12-15 16:18:38 +0800

    主界面功能完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,17 @@
-getUserInfi();
 function getUserInfi() {
   let token = localStorage.getItem('token');
-  console.log(token);
+  let layer = layui.layer;
+  // console.log(token);
   $.ajax({
     type: 'GET',
     url: '/my/userinfo',
-    headers: {
-      Authorization: localStorage.getItem('token')
-    },
+    // headers: {
+    //   Authorization: localStorage.getItem('token')
+    // },
     success: function (res) {
-      console.log(res);
-      if (res.status == 1) {
-        return alert('网络错误');
+      // console.log(res);
+      if (res.status !== 0) {
+        return layer.msg('用户信息获取失败');
       }
       if (res.data.user_pic == null) {
         $('.layui-nav-img').hide();
@@ -24,8 +24,30 @@ function getUserInfi() {
         $('.userPortrait').hide();
       }
       let UIF = res.data.nickname || res.data.username;
-      console.log(UIF);
+      // console.log(UIF);
+
       $('.userInfoName').text(UIF);
+    },
+    complete: function (x) {
+      console.log(x.responseJSON.status);
+      console.log(x.responseJSON.message);
+      if (x.responseJSON.status !== 0 || x.responseJSON.message == '身份认证成功!') {
+        console.log('失败了');
+        localStorage.removeItem('token');
+        location.href = 'http://127.0.0.1:5500/home/login.html';
+      }
     }
   });
 }
+getUserInfi();
+let layer = layui.layer;
+
+$('#logoutBtn').on('click', function () {
+  console.log(111);
+  layer.confirm('确定要退出吗?', { icon: 3, title: '提示' }, function (index) {
+    //do something
+    location.href = 'http://127.0.0.1:5500/home/login.html';
+    localStorage.removeItem('token');
+    layer.close(index);
+  });
+});
